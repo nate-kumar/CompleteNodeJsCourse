d976e07ce53ead4e36a4aaa99a4ffeb64c2f2fd0
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-09 21:02:45 +0000

    Basic mocha test cases for toBe, toBeA, toInclude

--- Section 6  - Testing Your Application/utils/utils.test.js
+++ Section 6  - Testing Your Application/utils/utils.test.js
@@ -1,17 +1,40 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
+  expect(res).toBe(9).toBeA('number');
 });
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(12)
+  // expect({name: 'Nathan'}).toBe({name: 'Nathan'})
+  // expect([1,2,3]).toEqual([1,2,3])
+  // expect([2,3,4]).toInclude(2);
+  // expect([2,3,4]).toExclude(1);
+  expect({
+    name: 'Nathan',
+    age: 29,
+    location: 'Philadelphia'
+  }).toInclude({
+    age: 29
+  })
+})
+
+it('should check for first and last name', () => {
+  var user = {
+    age: 29,
+    location: 'Philadelphia'
+  };
+  var res = utils.setName(user, 'Nathan Kumar');
+  
+  expect(res).toInclude({firstName: 'Nathan', lastName: 'Kumar'});
+})
