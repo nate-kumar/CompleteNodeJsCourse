00c458d03cb91ec54c29b4d96548c58da2023632
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-09 21:18:47 +0000

    Async tests asyncAdd, asyncSquare

--- Section 6  - Testing Your Application/utils/utils.test.js
+++ Section 6  - Testing Your Application/utils/utils.test.js
@@ -8,11 +8,25 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  })
+})
+
 it('should square a number', () => {
   var res = utils.square(3);
 
   expect(res).toBe(9).toBeA('number');
 });
+
+it('should async add two numbers', (done) => {
+  utils.asyncSquare(3, (res) => {
+    expect(res).toBe(9).toBeA('number');
+    done();
+  })
+})
 
 it('should expect some values', () => {
   // expect(12).toNotBe(12)
