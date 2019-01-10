933b82b79004cc3f478d70001908da8f370ab4bb
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-10 23:07:03 +0000

    Implemented describe()

--- Section 6  - Testing Your Application/utils/utils.test.js
+++ Section 6  - Testing Your Application/utils/utils.test.js
@@ -2,31 +2,35 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
-
-  expect(res).toBe(44).toBeA('number');
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+describe('Utils', () => {
+  it('should add two numbers', () => {
+    var res = utils.add(33, 11);
+  
+    expect(res).toBe(44).toBeA('number');
+  });
+  
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    })
   })
-})
-
-it('should square a number', () => {
-  var res = utils.square(3);
-
-  expect(res).toBe(9).toBeA('number');
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncSquare(3, (res) => {
+  
+  it('should square a number', () => {
+    var res = utils.square(3);
+  
     expect(res).toBe(9).toBeA('number');
-    done();
+  });
+  
+  it('should async add two numbers', (done) => {
+    utils.asyncSquare(3, (res) => {
+      expect(res).toBe(9).toBeA('number');
+      done();
+    })
   })
 })
+
+
 
 it('should expect some values', () => {
   // expect(12).toNotBe(12)
