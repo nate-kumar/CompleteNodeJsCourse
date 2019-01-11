4b9e85f905567d4aa1cd132b8046fa6bd8bd5da7
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-11 07:55:00 +0000

    applied describe() to tests in server.test.js

--- Section 6  - Testing Your Application/server/server.test.js
+++ Section 6  - Testing Your Application/server/server.test.js
@@ -3,30 +3,38 @@ const expect = require('expect')
 
 const app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    // .expect({
-    //     error: 'Page not found'
-    // })
-    .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'Page not found.'
-        })
-
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+            .get('/')
+            .expect(404)
+            // .expect({
+            //     error: 'Page not found'
+            // })
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'Page not found.'
+                })
+        
+            })
+            .end(done);
+        })        
     })
-    .end(done);
+
+    describe('GET /users', () => {
+        it('should check that users in the array exist', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body[0]).toInclude({
+                    name: 'Nathan'
+                })
+            })
+            .end(done)
+        })
+    })
 })
 
-it('should check that users in the array exist', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body[0]).toInclude({
-            name: 'Nathan'
-        })
-    })
-    .end(done)
-})
+
