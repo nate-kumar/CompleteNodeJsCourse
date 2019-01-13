1741baf0204096362c5db63c50efc69b51e05bdb
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-13 22:15:26 +0000

    Added / tested with spies

---
+++ Section 6  - Testing Your Application/spies/app.test.js
@@ -0,0 +1,27 @@
+const expect = require('expect');
+const rewire = require('rewire')
+
+let app = rewire('./app')
+// app.__set__
+// app.__get__
+
+describe('App', () => {
+    let db = {
+        saveUser: expect.createSpy()
+    };
+    app.__set__('db', db)
+
+    it('should call the spy correctly', () => {
+        let spy = expect.createSpy();
+        spy('Andrew', 25);
+        expect(spy).toHaveBeenCalledWith('Andrew', 25);
+    })
+
+    it('should call saveUser with user object', () => {
+        let email = 'andrew@example.com'
+        let password = '123abc'
+
+        app.handleSignup(email, password)
+        expect(db.saveUser).toHaveBeenCalledWith({email, password})
+    })
+})
