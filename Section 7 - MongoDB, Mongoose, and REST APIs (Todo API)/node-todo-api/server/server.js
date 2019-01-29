9b63c027c9aa414edff40f3866889bd7082c7936
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-29 20:16:34 +0000

    async test cases written to verify POST behaviour when writing to database

--- Section 7 - MongoDB, Mongoose, and REST APIs (Todo API)/node-todo-api/server/server.js
+++ Section 7 - MongoDB, Mongoose, and REST APIs (Todo API)/node-todo-api/server/server.js
@@ -22,6 +22,8 @@ app.post('/todos', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000')
+app.listen(3001, () => {
+    console.log('Started on port 3001')
 })
+
+module.exports = {app};
