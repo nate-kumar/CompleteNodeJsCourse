1d107d0c1f34ee8668f29c0b40dbe1dd9a011f7b
Nate Kumar <nathan.kumar@hotmail.co.uk>
2019-01-29 07:49:30 +0000

    Refactored models into own files, POST data using express and postman

--- Section 7 - MongoDB, Mongoose, and REST APIs (Todo API)/node-todo-api/server/server.js
+++ Section 7 - MongoDB, Mongoose, and REST APIs (Todo API)/node-todo-api/server/server.js
@@ -1,38 +1,27 @@
-const mongoose = require('mongoose');
+let express = require('express');
+let bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
 
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    },
-})
+var app = express();
 
-// let newTodo = new Todo({
-//     text: 'Cook dinner'
-// });
+app.use(bodyParser.json())
 
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (err) => {
-//     console.log('Unable to save todo', err);
-// });
+app.post('/todos', (req, res) => {
+    // console.log(req.body);
+    let todo = new Todo({
+        text: req.body.text
+    })
 
-let newTodo2 = new Todo({
-    text: 'Do washing',
-    completed: true,
-    completedAt: 5
-})
-
-newTodo2.save().then((doc) => {
-    console.log('Saved todo', doc);
-}, (err) => {
-    console.log('Unable to save todo', err);
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.status(400).send(err);
+    })
 });
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
+})
